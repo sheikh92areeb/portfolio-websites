ecf4725ff636336fb8b1a8e61225eaedaded9c89
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-01-26 13:36:30 +0500

    update

---
+++ portfolio-5/assets/js/script.js
@@ -0,0 +1,7 @@
+/*============================== typing animation ==============================*/
+var typed = new Typed(".typing",{
+	strings:["","Web Designer", "Web Developer", "Freelancer", "Digital Marketer"],
+	typeSpeed:100,
+	backSpeed:60,
+	loop:true
+})
